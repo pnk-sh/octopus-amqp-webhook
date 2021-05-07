42b6d6d28a956ab2aae04654b317e80d433aa1c4
Paris Nakita Kejser <contact@parisnakitakejser.com>
2021-05-07 08:36:08 +0200

    add better debug logging for developering

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ amqp.connect(`${process.env.AMQP_HOST}`, function(error0: any, connection: any) 
     channel.consume(queue, (msg: any) => {
       const json_data = JSON.parse(msg.content.toString());
 
-      logger.info(" [x] Received %s", json_data);
+      logger.debug(`[x] Received ${json_data}`);
 
       const deploy_queue = process.env.AMQP_QUEUE_DEPLOYMENT;
 
@@ -40,6 +40,8 @@ amqp.connect(`${process.env.AMQP_HOST}`, function(error0: any, connection: any) 
         const service_count = resp.data.services.length
 
         if (service_count > 0) {
+          logger.info(`${service_count} service found with identifier: ${json_data.identifier} and tag:${json_data.tag}`)
+
           axios.post(`${process.env.REST_API_HOST}webhook/${json_data.id}`, {
             status: 'processing',
             service_update_pending: service_count
@@ -61,7 +63,7 @@ amqp.connect(`${process.env.AMQP_HOST}`, function(error0: any, connection: any) 
               });
 
               channel.sendToQueue(`${deploy_queue}-${cluster_id}`, Buffer.from(deploy_msg));
-              console.log(" [x] Sent %s", deploy_msg);
+              logger.debug(`[x] Sent ${deploy_msg}`)
 
               let utctime = (new Date()).toISOString();
               utctime = utctime.replace('T', ' ')
@@ -82,6 +84,7 @@ amqp.connect(`${process.env.AMQP_HOST}`, function(error0: any, connection: any) 
             }
           });
         } else {
+          logger.info(`No service found with identifier:${json_data.identifier} and tag:${json_data.tag}`)
           axios.post(`${process.env.REST_API_HOST}webhook/${json_data.id}`, {
             status: 'cancel',
           })
