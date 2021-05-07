c9d0df29d41b97069b219028c66043f50f1a6bf2
Paris Nakita Kejser <contact@parisnakitakejser.com>
2021-05-07 13:02:13 +0200

    remove identifier, can be found based on webhook_id

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,6 @@ amqp.connect(`${process.env.AMQP_HOST}`, function(error0: any, connection: any) 
                 "cluster_id": cluster_id,
                 "service_id": service_data.ID,
                 "webhook_id": json_data.id,
-                "webhook_identifier": json_data.identifier,
                 "image": service_data.Image,
                 "tag": json_data.tag
               });
